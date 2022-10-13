0fa894fda0deaa2b206baa15b5d4f5541b932e25
Flood99 <113184223+Flood99@users.noreply.github.com>
2022-10-13 08:32:58 -0400

    rubyMoves

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public int spd = 3;
+    float Horizontal;
+    float Vertical;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        Horizontal = Input.GetAxis("Horizontal");
+        Vertical = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(Horizontal * spd * Time.deltaTime,Vertical*spd*Time.deltaTime,0));
     }
 }
